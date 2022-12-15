68a59631c6af6fc69eb44ac8247756d2cf81eca8
Brenda Monis Moreno <91157764+BrendaMonis@users.noreply.github.com>
2022-12-15 14:08:44 -0300

    Update notas.py

--- notas.py
+++ notas.py
@@ -6,12 +6,12 @@ TOTAL = 0
 contador_notas = 0
 
 #Realizando o controle de inserção do usuário
-n = int(input("Digite a quantidade de células da sua lista: "))
+n = int(input("Informe a quantidade de alunos na turma: "))
 cont = 1
 notas = []
 
 for i in range(n):
-        notas += input("Informe o valor de x%d: " % (i+1))
+        notas += input("Informe a nota do aluno x%d: " % (i+1))
 
 vetor_original = notas
 
