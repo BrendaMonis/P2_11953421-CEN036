380f2ca0d141ae2879978b36ade01216b9be4fb2
Brenda Monis Moreno <91157764+BrendaMonis@users.noreply.github.com>
2022-12-15 14:04:02 -0300

    Update notas.py

--- notas.py
+++ notas.py
@@ -1,1 +1,18 @@
-a
+#!/usr/bin/env python3
+
+import sys
+
+TOTAL = 0
+contador_notas = 0
+
+#Realizando o controle de inserção do usuário
+n = int(input("Digite a quantidade de células da sua lista: "))
+cont = 1
+notas = []
+
+for i in range(n):
+        notas += input("Informe o valor de x%d: " % (i+1))
+
+vetor_original = notas
+
+print(f"Vetor Original {vetor_original}")
