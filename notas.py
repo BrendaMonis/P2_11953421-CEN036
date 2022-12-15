90a01c9ce836548121e64d5d574babf17222eeba
Brenda Monis Moreno <91157764+BrendaMonis@users.noreply.github.com>
2022-12-15 14:28:33 -0300

    Update notas.py

--- notas.py
+++ notas.py
@@ -6,13 +6,27 @@ TOTAL = 0
 contador_notas = 0
 
 #Realizando o controle de inserção do usuário
-n = int(input("Informe a quantidade de alunos na turma: "))
+n = int(input("Informe a quantidade de nota: "))
 cont = 1
 notas = []
 
 for i in range(n):
-        notas += input("Informe a nota do aluno x%d: " % (i+1))
+        notas += input("Informe cada nota x%d: " % (i+1))
 
 vetor_original = notas
 
 print(f"Vetor Original {vetor_original}")
+
+#Realizando a contagem das notas
+
+while contador_notas <= n:
+        contador_notas = contador_notas + 1
+        print(contador_notas)
+        TOTAL = TOTAL + notas #Somando as notas ao total
+        print(TOTAL)
+
+#Média da disciplina
+
+média = TOTAL/10
+print('a média da disciplina é:', média)
+
